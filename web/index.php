bfd2e3596e46fb92f48617b6de5111e9313cb93d
jambonbill <jambonbill@gmail.com>
2015-04-23 16:40:44 +0800

    rewrite

--- web/index.php
+++ web/index.php
@@ -17,7 +17,7 @@ echo $admin;//
 
 <section class="content">
   <div class="row">
-    <div class="col-md-4 col-sm-6 col-xs-12" >
+    <div class="col-md-6 col-sm-6 col-xs-12" >
     <?php
     $box= new Admin\Box;
     $box->icon("fa fa-question");
@@ -26,14 +26,14 @@ echo $admin;//
     $box->removable(true);
     
     $body=[];
-    $body[]="AdminLteTurbo is a php classed, striped down version of <a href='https://almsaeedstudio.com/'>Almaseed AdminLTE2</a> which is based on <a href='http://getbootstrap.com/'>bootstrap</a><br />";
+    $body[]="AdminLteTurbo is a php classed, striped down version of <a href='https://almsaeedstudio.com/'>Almaseed AdminLTE2</a> which is based on twitter <a href='http://getbootstrap.com/'>Bootstrap</a><br />";
     $body[]="The goal is to help develop web/php apps super quickly, with little dependencies.<br />";
-    $body[]="Plugins have been removed and only the required dependencies (Bootstap and jQuery) are left.<br />";
+    $body[]="Plugins have been removed and only the required dependencies (Bootstap and jQuery)<br />";
     $box->body($body);
     
     $foot=[];
-    $foot[]="<a href='config.php' class='btn btn-default'><i class='fa fa-wrench'></i> Configuration</a> ";
-    $foot[]="<a href='examples.php' class='btn btn-default'><i class='fa fa-list'></i> Examples</a> ";
+    $foot[]="<a href='examples/config.php' class='btn btn-default'><i class='fa fa-wrench'></i> Configuration</a> ";
+    $foot[]="<a href='examples/' class='btn btn-default'><i class='fa fa-list'></i> Examples</a> ";
     $box->footer($foot);
     
     echo $box;
@@ -45,6 +45,7 @@ echo $admin;//
     
     <div class="col-md-4 col-sm-6 col-xs-12">
     <?php
+    /*
     $box= new Admin\Box;
     $box->title("Layout");
     $box->body("Admin layout can defined in the config, and/or changed with javascript.<br />See dist/js/demo.js");
@@ -55,6 +56,7 @@ echo $admin;//
     $box->title("Examples");
     $box->body("The php classes provide helpers to build widgets very easily.<br />Examples :<pre></pre>");
     echo $box;
+    */
     ?>
 
     </div><!-- /.col -->
