caec3989c475562119092a4b68aa8da7faea378b
jambonbill <jambonbill@gmail.com>
2015-08-04 10:54:45 +0800

    Breadcrumbs removed

--- web/examples/icons.php
+++ web/examples/icons.php
@@ -12,10 +12,7 @@ echo $admin->html();//
     <i class="fa fa-apple"></i> Icons
     <small>Pretty icons</small>
   </h1>
-  <ol class="breadcrumb">
-    <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-    <li class="active">Here</li>
-  </ol>
+  
 </section>
 
 
