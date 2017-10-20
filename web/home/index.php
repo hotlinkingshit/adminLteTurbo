82ae4863d4cc0e4135e439201f4ffcc394d5ed08
jambonbill <jambonbill@gmail.com>
2017-10-20 21:30:25 +0800

    <h1>Hi</h1>

--- web/home/index.php
+++ web/home/index.php
@@ -14,62 +14,26 @@ echo $admin;//
 </section>
 
 <section class="content">
+  <h1>Hi</h1>
   <div class="row">
     <div class="col-md-6 col-sm-6 col-xs-12" >
     <?php
-    $box= new LTE\Box;
-    $box->icon("fa fa-question");
-    $box->type("warning");
-    $box->title("About");
-    $box->removable(true);
-    $box->collapsable(true);
+    include "box_jambonbill.php";
 
-    $body=[];
-    $body[]="AdminLteTurbo is a php classed, striped down version of <a href='https://almsaeedstudio.com/'>Almaseed AdminLTE2</a> which is based on twitter <a href='http://getbootstrap.com/'>Bootstrap</a><br />";
-    $body[]="The goal is to help develop web/php apps super quickly, with little dependencies.<br />";
-    $body[]="Plugins have been removed and only the required dependencies (Bootstap and jQuery)<br />";
-    $box->body($body);
-
-    $foot=[];
-    $foot[]="<a href='examples/config.php' class='btn btn-default'><i class='fa fa-wrench'></i> Configuration</a> ";
-    $foot[]="<a href='examples/' class='btn btn-default'><i class='fa fa-list'></i> Examples</a> ";
-    $box->footer($foot);
-
-    echo $box;
     ?>
-
-
-
     </div><!-- /.col -->
 
     <div class="col-md-6 col-sm-6 col-xs-12">
     <?php
-
-
-    $box= new LTE\Box;
-    $box->title("Get started");
-    $htm=[];
-    $htm[]="<pre>\n";
-    $htm[]="&lt;?php\n";
-    $htm[]="header('Content-Type: text/html; charset=utf-8');\n";
-    $htm[]="require __DIR__.'/../vendor/autoload.php';\n";
-    $htm[]="\n";
-    $htm[]="\$admin = new LTE\AdminLte2();\n";
-    $htm[]="\$admin->title('My title');\n";
-    $htm[]="\n";
-    $htm[]="echo \$admin;\n";
-    $htm[]="</pre>";
-    $box->body($htm);
-    $box->footer("<a href='examples/config.php' class='btn btn-default'>Ok</a> ");
-    $box->collapsable(1);
-    echo $box;
-
+    include "box_about.php";
+    //include "box_getStarted.php";
+    include "box_ajax.php";
     ?>
-
     </div><!-- /.col -->
 
   </div><!-- /.row -->
 </section>
+<script src="js/home.js"></script>
 <?php
 $admin->footer("<a href='http://adminlteturbo.jambonbill.org'>AdminLteTurbo</a>");//define footer
 $admin->end();
