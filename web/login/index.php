82b224e7801fb6f61a86466e3ee71f6e5fdb8060
jambonbill <jambonbill@gmail.com>
2017-10-20 21:30:47 +0800

    less crap

--- web/login/index.php
+++ web/login/index.php
@@ -31,12 +31,8 @@ echo $admin->html();//
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
-        <div class="col-xs-8">    
-          <div class="checkbox icheck">
-            <label>
-              <div class="icheckbox_square-blue" aria-checked="false" aria-disabled="false" style="position: relative;"><input type="checkbox" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; border: 0px; opacity: 0; background: rgb(255, 255, 255);"><ins class="iCheck-helper" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; border: 0px; opacity: 0; background: rgb(255, 255, 255);"></ins></div> Remember Me
-            </label>
-          </div>                        
+        <div class="col-xs-8">
+
         </div><!-- /.col -->
         <div class="col-xs-4">
           <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
@@ -44,7 +40,7 @@ echo $admin->html();//
       </div>
     </form>
 
-    
+
     <a href="#">I forgot my password</a><br>
     <a href="register.html" class="text-center">Register a new membership</a>
 
@@ -52,16 +48,6 @@ echo $admin->html();//
 </div>
 
 
-<div class='row'>
-	<div class='col-md-4'></div>
-	<div class='col-md-4'>
-		A login page example, the menu has been collapsed with :
-		<pre>&lt;script>
-$("body").toggleClass('sidebar-collapse');
-&lt;/script></pre>
-	</div>
-	<div class='col-md-4'></div>
-</div>
 
 <script>
 $(function(){
